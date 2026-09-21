23f6b101ef9000f194cc7eecd781bc9be9ed7172
kronen9999 <stacica22eneronkio@gmail.com>
2026-09-21 09:57:40 -0600

    Se han agregado la libreria comon como dependencia a los ms , tambien se cambio de jpa para cada servicio independiente

--- common/src/main/java/com/team/common/exceptions/GlobalExceptionHandler.java
+++ common/src/main/java/com/team/common/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.team.common.exceptions;
 
-import com.carlos.commons.dto.CustomErrorResponse;
+import com.team.common.dto.CustomErrorResponse;
 import feign.FeignException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
--- habitaciones/src/main/java/com/team/habitaciones/HabitacionesApplication.java
+++ habitaciones/src/main/java/com/team/habitaciones/HabitacionesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.team.habitaciones", "com.team.common"})
 public class HabitacionesApplication {
 
 	public static void main(String[] args) {
