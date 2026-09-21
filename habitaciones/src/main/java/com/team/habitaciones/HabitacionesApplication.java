fcbd67cc2cb01ac4f3d02f53db209163bc9c5d66
kronen9999 <stacica22eneronkio@gmail.com>
2026-09-21 09:16:46 -0600

    Se han configurado los aplication properties tanto de habitaciones , huespedes y reservas para que el eureka client los reconozca,tambien se ha ajustado el yml de el gateway para que apunte a la raiz de cada microservicio

--- habitaciones/src/main/java/com/team/habitaciones/HabitacionesApplication.java
+++ habitaciones/src/main/java/com/team/habitaciones/HabitacionesApplication.java
@@ -3,6 +3,9 @@ package com.team.habitaciones;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class HabitacionesApplication {
 
