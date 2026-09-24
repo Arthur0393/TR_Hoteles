7caad33758e27c6a1cf7026073d5e1d96ec5045d
kronen9999 <stacica22eneronkio@gmail.com>
2026-09-24 14:31:25 -0600

    faltaba un @Configuration

--- gateway/src/main/java/com/team/gateway/configuration/SecurityConfig.java
+++ gateway/src/main/java/com/team/gateway/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.team.gateway.configuration;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -12,6 +13,7 @@ import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
 
+@Configuration
 public class SecurityConfig {
 
 
